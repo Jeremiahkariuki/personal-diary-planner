906fc8bc7dd6d74f4e0d5e16fc41ba685cf7d958
Jeremiah Kariuki <jeremmykariz001@gmail.com>
2026-07-23 20:51:49 +0300

    Enable stderr/stdout logging in production settings

--- diary_planner_prj/settings.py
+++ diary_planner_prj/settings.py
@@ -317,3 +317,24 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
