7031b5b26f990bcb9e53bede80c3246c9ac1c3c1
Jeremiah Kariuki <jeremmykariz001@gmail.com>
2026-07-29 10:35:36 +0300

    setting

--- diary_planner_prj/settings.py
+++ diary_planner_prj/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
 RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append('https://jdiary-mulw.onrender.com/')
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 if DEBUG:
     ALLOWED_HOSTS += ['localhost', '127.0.0.1', '[::1]', 'testserver']
